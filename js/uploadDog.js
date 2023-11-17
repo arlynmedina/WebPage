e77ae720c74c541b3ca5c80e673e7a16a8a0fdb2
arlynmedina <arlynlinette@gmail.com>
2023-11-16 21:15:18 -0600

    Pre-proyecto

--- js/uploadDog.js
+++ js/uploadDog.js
@@ -53,14 +53,3 @@ function agregarInput() {
       }
   }
 
-let prevPos = window.pageYOffset;
-
-window.onscroll = function(){
-    let currentPos = window.pageYOffset;
-    if(prevPos > currentPos){
-        document.getElementById("mainNav").style.top = "0";
-    }else{
-        document.getElementById("mainNav").style.top = "-100px"
-    }
-    prevPos = currentPos;
-}
