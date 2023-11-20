d42c81e0acf6dd0ba7deb5fa0142396aae50e7de
XimeIsaac <ximena.isaac@iteso.mx>
2023-11-20 00:07:54 -0600

    pagination

--- js/index.js
+++ js/index.js
@@ -1,101 +1,48 @@
-const selectedValue = sessionStorage.getItem("selectedValue");
+const itemsPerPage = 6; 
+let currentPage = 1; 
 
-function showDogInfoIndex(selectedValue) {
+function showDogInfoIndex(selectedValue, page) {
     let xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://localhost:3000/dogs/' + selectedValue);
+    let startIndex = (page - 1) * itemsPerPage;
+    let endIndex = startIndex + itemsPerPage;
+
+    xhr.open('GET', 'http://localhost:3000/dogs');
     xhr.send();
+
     let item = '';
     let dogInfoIndex = document.getElementById('dogInfoIndex');
-    console.log(dogInfoIndex);
+    
     xhr.onload = function () {
         let dogs = JSON.parse(xhr.response);
-        item += `<div class="row">
-        <div class="col-lg-4 col-sm-6 mb-4">
-            <div class="portfolio-item">
-                <a class="portfolio-link" href="/WebPage/views/dogDescription.html">
-                    <div class="portfolio-hover">
-                        <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
-                    </div>
-                    <img class="img-fluid" src="${dogs.imagen}" alt="..." />
-                </a>
-                <div class="portfolio-caption">
-                    <div class="portfolio-caption-heading">${dogs.nombre}</div>
-                    <div class="portfolio-caption-subheading text-muted">${dogs.raza}</div>
-                </div>
-            </div>
-        </div>
-            <div class="col-lg-4 col-sm-6 mb-4">
+
+        endIndex = Math.min(endIndex, dogs.length);
+
+        for (let i = startIndex; i < endIndex; i++) {
+            item += `<div class="col-lg-4 col-sm-6 mb-4">
                 <div class="portfolio-item">
                     <a class="portfolio-link" href="/WebPage/views/dogDescription.html">
                         <div class="portfolio-hover">
                             <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
                         </div>
-                        <img class="img-fluid" src="${dogs.imagen}" alt="..." />
+                        <img class="img-fluid" src="${dogs[i].imagen}" alt="..." />
                     </a>
                     <div class="portfolio-caption">
-                        <div class="portfolio-caption-heading">${dogs.nombre}</div>
-                        <div class="portfolio-caption-subheading text-muted">${dogs.raza}</div>
+                        <div class="portfolio-caption-heading">${dogs[i].nombre}</div>
+                        <div class="portfolio-caption-subheading text-muted">${dogs[i].raza}</div>
                     </div>
                 </div>
-            </div>
-            <div class="col-lg-4 col-sm-6 mb-4">
-                <div class="portfolio-item">
-                    <a class="portfolio-link" href="/WebPage/views/dogDescription.html">
-                        <div class="portfolio-hover">
-                            <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
-                        </div>
-                        <img class="img-fluid" src="${dogs.imagen}" alt="..." />
-                    </a>
-                    <div class="portfolio-caption">
-                        <div class="portfolio-caption-heading">${dogs.nombre}</div>
-                        <div class="portfolio-caption-subheading text-muted">${dogs.raza}</div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-sm-6 mb-4 mb-lg-0">
-                <div class="portfolio-item">
-                    <a class="portfolio-link" href="/WebPage/views/dogDescription.html">
-                        <div class="portfolio-hover">
-                            <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
-                        </div>
-                        <img class="img-fluid" src="${dogs.imagen}" alt="..." />
-                    </a>
-                    <div class="portfolio-caption">
-                        <div class="portfolio-caption-heading">${dogs.nombre}</div>
-                        <div class="portfolio-caption-subheading text-muted">${dogs.raza}</div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-sm-6 mb-4 mb-sm-0">
-                <div class="portfolio-item">
-                    <a class="portfolio-link" href="/WebPage/views/dogDescription.html">
-                        <div class="portfolio-hover">
-                            <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
-                        </div>
-                        <img class="img-fluid" src="${dogs.imagen}" alt="..." />
-                    </a>
-                    <div class="portfolio-caption">
-                        <div class="portfolio-caption-heading">${dogs.nombre}</div>
-                        <div class="portfolio-caption-subheading text-muted">${dogs.raza}</div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-sm-6">
-                <div class="portfolio-item">
-                    <a class="portfolio-link" href="/WebPage/views/dogDescription.html">
-                        <div class="portfolio-hover">
-                            <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
-                        </div>
-                        <img class="img-fluid" src="${dogs.imagen}" alt="..." />
-                    </a>
-                    <div class="portfolio-caption">
-                        <div class="portfolio-caption-heading">${dogs.nombre}</div>
-                        <div class="portfolio-caption-subheading text-muted">${dogs.raza}</div>
-                    </div>
-                </div>
-            </div>
-        </div>`
+            </div>`;
+        }
+
         dogInfoIndex.innerHTML = item;
     }
 }
-showDogInfoIndex(selectedValue);
+
+function paginate(page) {
+    currentPage = page;
+    showDogInfoIndex(selectedValue, currentPage);
+}
+
+buttonElement.addEventListener('click', function() {
+    paginate(2); 
+});
