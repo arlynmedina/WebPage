0fc9785c5291aa4d61c1c6016ba9cda0488de347
XimeIsaac <ximena.isaac@iteso.mx>
2023-11-20 20:58:26 -0600

    pagination final ahora sí

--- js/index.js
+++ js/index.js
@@ -1,41 +1,62 @@
-const itemsPerPage = 6; // Number of items to display per page
-let currentPage = 1;
 
-function showDogInfoIndex(selectedValue, page) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', `http://localhost:3000/dogs/${selectedValue}?_page=${page}&_limit=${itemsPerPage}`);
+function showDogInfoIndex(pageNumber = 1) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'http://localhost:3000/dogs');
     xhr.send();
-    let item = '';
+    
+    const dogsPerPage = 6; 
+    let items = '';
     let dogInfoIndex = document.getElementById('dogInfoIndex');
-    console.log(dogInfoIndex);
+    let pagination = document.getElementById('pagination');
+
     xhr.onload = function () {
         let dogs = JSON.parse(xhr.response);
-        dogs.forEach(dog => {
-            item += `<div class="col-lg-4 col-sm-6 mb-4">
-                        <div class="portfolio-item">
-                            <a class="portfolio-link" href="/WebPage/views/dogDescription.html">
-                                <div class="portfolio-hover">
-                                    <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
-                                </div>
-                                <img class="img-fluid" src="${dog.imagen}" alt="..." />
-                            </a>
-                            <div class="portfolio-caption">
-                                <div class="portfolio-caption-heading">${dog.nombre}</div>
-                                <div class="portfolio-caption-subheading text-muted">${dog.raza}</div>
-                            </div>
-                        </div>
-                    </div>`;
-        });
 
-        dogInfoIndex.innerHTML = item;
+        const startIndex = (pageNumber - 1) * dogsPerPage;
+        const endIndex = startIndex + dogsPerPage;
+
+        for (let i = startIndex; i < endIndex && i < dogs.length; i++) {
+            const dog = dogs[i];
+            items += `<div class="col-lg-4 col-sm-6 mb-4">
+                <div class="portfolio-item">
+                    <a class="portfolio-link" href="/WebPage/views/dogDescription.html">
+                        <div class="portfolio-hover">
+                            <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+                        </div>
+                        <img class="img-fluid" src="${dog.imagen}" alt="..." />
+                    </a>
+                    <div class="portfolio-caption">
+                        <div class="portfolio-caption-heading">${dog.nombre}</div>
+                        <div class="portfolio-caption-subheading text-muted">${dog.raza}</div>
+                    </div>
+                </div>
+            </div>`;
+        }
+
+        items = `<div class="row">${items}</div>`;
+        dogInfoIndex.innerHTML = items;
+
+        const totalPages = Math.ceil(dogs.length / dogsPerPage);
+        generatePaginationLinks(totalPages, pageNumber);
     }
 }
+function generatePaginationLinks(totalPages, currentPage) {
+    let pagination = document.getElementById('pagination');
+    let paginationHTML = '';
 
+    for (let i = 1; i <= totalPages; i++) {
+        if (i === currentPage) {
+            paginationHTML += `<li class="active">${i}</li>`;
+        } else {
+            paginationHTML += `<li onclick="showDogInfoIndex(${i})">${i}</li>`;
+        }
+    }
 
-function changePage(page) {
-    currentPage = page;
-    showDogInfoIndex(selectedValue, currentPage);
+    pagination.innerHTML = paginationHTML;
 }
+showDogInfoIndex();
 
 
-showDogInfoIndex(selectedValue, currentPage);
+
+
+
